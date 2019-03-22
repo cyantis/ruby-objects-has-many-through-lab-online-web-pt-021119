7115015ab7c2c731c1d467d12227b0fbec93b124
Cody Yantis <cyantis@gmail.com>
2019-03-22 21:14:34 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,4 +12,12 @@ class Doctor
     Appointment.new(date, patient, self)
   end
 
+  def appointments
+    Appointment.all.select {|d| d.doctor == self}
+  end
+
+  def patients
+    Appointment.all.find_all{|d| d.doctor == self}.coll
+  end
+
 end
