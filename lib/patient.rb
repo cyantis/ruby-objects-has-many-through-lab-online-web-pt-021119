9d8e7e62ed50e9036aeb3e5da8dab4cb3bcdbc21
Cody Yantis <cyantis@gmail.com>
2019-03-22 21:17:24 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,19 @@
+class Patient
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def appointments
+    Appointment.all.select {|p| p.patient == self}
+  end
+
+  def patients
+    Appointment.all.find_all{|p| p.patient == self}.collect {|d| d.doctor}
+  end
+
+end
