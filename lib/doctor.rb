fb5ed5930306e9a71dc721a3e766476556d0457b
Cody Yantis <cyantis@gmail.com>
2019-03-22 21:11:20 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,10 +1,15 @@
 class Doctor
   attr_accessor :name
+  
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
   end
 
 end
