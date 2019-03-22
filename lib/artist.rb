6f06f01ecc7008fee34b89b24655cf873431c0b5
Cody Yantis <cyantis@gmail.com>
2019-03-22 20:41:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
 
   def initialize(name)
     @name = name
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
--- lib/genre.rb
+++ lib/genre.rb
@@ -5,7 +5,7 @@ class Genre
 
   def initialize(name)
     @name = name
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
